4c9c8566a614b0c018e4dd137cac1bddd73ab66a
abhipanda4 <abhipanda03@gmail.com>
2018-06-10 15:25:56 +0530

    img saving added, step size change rule modified

--- one_plus_one_ES.py
+++ one_plus_one_ES.py
@@ -8,6 +8,7 @@ N_GEN = 100
 DNA_BOUNDS = [0, 5]
 T = 5
 TAU = 0.9
+SAVE_PATH = "figs/1_plus_1/"
 
 def F(x):
     return np.sin(10*x)*x + np.cos(2*x)*x
@@ -29,16 +30,19 @@ def mutate(x_parent, sigma):
 def adjust_step_size(sigma, num_success):
     success_ratio = num_success / T
     target = 0.2
-    x = 0
     if success_ratio > target:
-        x = 1
-    return sigma * np.exp(1/np.sqrt(DNA_SIZE+1) * (x - target)/(1 - target))
+        sigma = sigma / TAU
+    else:
+        sigma = sigma * TAU
+    return sigma
 
 def main():
     # plotting
     plt.ion()
     x_plot = np.linspace(DNA_BOUNDS[0], DNA_BOUNDS[1], 100)
     y_plot = np.array([F(x) for x in x_plot])
+    if not os.path.exists(SAVE_PATH):
+        os.mkdir(SAVE_PATH)
 
     # initialize root parent randomly
     X = DNA_BOUNDS[1] * np.random.rand(DNA_SIZE)
@@ -51,6 +55,7 @@ def main():
     num_success = 0
 
     for g in range(1, N_GEN + 1):
+        print("Generation: [%d]" %(g))
         if g % T == 0:
             sigma = adjust_step_size(sigma, num_success)
             num_success = 0
@@ -63,10 +68,11 @@ def main():
 
         # plot child and kid
         plt.cla()
-        plt.scatter(X, F(X), color='b', lw=5, alpha=0.3)
-        plt.scatter(X_prime, F(X_prime), color='g', lw=5, alpha=0.3)
+        plt.scatter(X, F(X), color='g', lw=5, alpha=0.5)
+        plt.scatter(X_prime, F(X_prime), color='r', lw=5, alpha=0.3)
         plt.plot(x_plot, y_plot)
         plt.pause(0.05)
+        plt.savefig(SAVE_PATH + "1_plus_1_gen_" + str(g) + ".png")
 
         # current child is the next genration's parent
         X = X_prime
